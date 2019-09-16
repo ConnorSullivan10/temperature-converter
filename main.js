45dc360961d29ad7e2aa15f7f70f68416c20b6b2
Connor Sullivan <cksullvn@gmail.com>
2019-09-15 21:11:18 -0500

    created basic layout of temp converter page without the functions display: table-cell; or event listeners completed   text-align: center;

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 console.log('hi');
+
+const clearButton = document.getElementById("clear");
+const convertButton = document.getElementById("convert");
+
+clearButton.addEventListener
+convertButton.addEventListener
+
+const toCelsius =  () => {
+
+}
+
+const toFahrenheit =  () => {
+
+}
+
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+const determineConverter = (e) => {
+    const fCheck = document.getElementById("myRadioF").checked;
+    const cCheck = document.getElementById("myRadioC").checked;
+    console.log("event", e);
+  }
+  // Assign a function to be executed when the button is clicked
+  button.addEventListener("click", determineConverter);
+
