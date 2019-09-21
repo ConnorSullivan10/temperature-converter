9a04287b8cf51781e8ed96d7eefb2db0c5ca566b
Connor Sullivan <cksullvn@gmail.com>
2019-09-20 21:06:44 -0500

    created layout for Temperature converter in html + css and created non-working functions in js to convert the temperature without yet creating the determineColor function

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
 console.log('hi');
 
-const clearButton = document.getElementById("clear");
-const convertButton = document.getElementById("convert");
+const printToDom = (toPrint, divId) => {
+  document.getElementById(divId).innerHTML = toPrint;
+}
 
-clearButton.addEventListener
-convertButton.addEventListener
+let formInput = document.getElementById("exampleFormControlInput1").value;
 
 const toCelsius =  () => {
-
-}
+  ((formInput-32)*5/9).join(" °C");
+};
 
 const toFahrenheit =  () => {
-
-}
+  (formInput*9/5+32).join(" °F");
+};
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 const determineConverter = (e) => {
-    const fCheck = document.getElementById("myRadioF").checked;
-    const cCheck = document.getElementById("myRadioC").checked;
-    console.log("event", e);
+  const fCheck = document.getElementById("myRadioF").checked;
+  const cCheck = document.getElementById("myRadioC").checked;
+  let domString = ``;
+  // console.log("event", e);
+  if (fCheck) {
+    domString = toFahrenheit();
+  } else if (cCheck) {
+    domString = toCelsius();
   }
-  // Assign a function to be executed when the button is clicked
-  button.addEventListener("click", determineConverter);
+  printToDom(domString, "convertedTemp")
+};
+console.log(determineConverter);
 
+  const clearConverter = (e) => {
+    printToDom([], "convertedTemp");
+    formInput == 0;
+  }
+
+  // Assign a function to be executed when the button is clicked
+  const convertButton = document.getElementById("convert");
+  const clearButton = document.getElementById("clear");
+
+  convertButton.addEventListener("click", determineConverter);
+  clearButton.addEventListener("click", clearConverter);
